bacad42a606dffd2118e300cc9e9c3d5a8c01ac6
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-05-09 13:40:50 -0500

    setup completed

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>INSIDE APP COMPONENT</h2>
+        <button className="btn btn-info">CRAP</button>
+      </div>
+    );
+  }
+}
+
+export default App;
