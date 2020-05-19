aced10a9736eda7bf3ca0525f0e633fbad134439
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-05-18 20:46:02 -0500

    added initial components

---
+++ src/components/TeamComponent/TeamComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './TeamComponent.scss';
+
+class TeamComponent extends React.Component {
+  render() {
+    return (
+      <div className="ReactComponent">
+        <h1>TEAM COMPONENT</h1>
+      </div>
+    );
+  }
+}
+
+export default TeamComponent;
